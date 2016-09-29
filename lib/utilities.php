75d46b582f952e3aec8879b17b3804a589467568
scossar <scossar3@gmail.com>
2016-09-29 13:04:20 -0700

    Consider 'false' equal to false

--- lib/utilities.php
+++ lib/utilities.php
@@ -7,7 +7,23 @@ use WPDiscourse\Utilities\Utilities as DiscourseUtilities;
 class Utilities {
 
 	public function get_options() {
+
 		return DiscourseUtilities::get_options();
+	}
+
+	public function base_url( array $options ) {
+
+		return ! empty( $options['url'] ) ? $options['url'] : '';
+	}
+
+	public function get_url( $base_url, $login = false, $return_path = '' ) {
+		if ( ! $login || 'false' === $login ) {
+			$url = $base_url . $return_path;
+		} else {
+			$url = $base_url . '/session/sso?return_path=' . $return_path;
+		}
+
+		return $url;
 	}
 
 }
