2eb3c2307a8051a8ef137af3eeb590dbe284431a
scossar <scossar3@gmail.com>
2016-10-02 02:38:22 -0700

    Use the title

--- lib/discourse-remote-message.php
+++ lib/discourse-remote-message.php
@@ -12,7 +12,6 @@ class DiscourseRemoteMessage {
 		$this->utilities = $utilities;
 
 		add_action( 'init', array( $this, 'setup' ) );
-//		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
 		add_shortcode( 'discourse_remote_message', array( $this, 'discourse_remote_message' ) );
 		add_action( 'wp_ajax_process_discourse_remote_message', array(
 			$this,
@@ -27,12 +26,6 @@ class DiscourseRemoteMessage {
 	public function setup() {
 		$this->options  = $this->utilities->get_options();
 		$this->base_url = $this->utilities->base_url( $this->options );
-	}
-
-	public function enqueue_scripts() {
-		wp_register_script( 'handle_remote_message_submission_js', plugins_url( '../js/handle-remote-message-submission.js', __FILE__ ), array( 'jquery' ) );
-		wp_localize_script( 'handle_remote_message_submission_js', 'handle_remote_message_submission_script', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
-		wp_enqueue_script( 'handle_remote_message_submission_js' );
 	}
 
 	public function discourse_remote_message( $atts ) {
@@ -86,27 +79,19 @@ class DiscourseRemoteMessage {
 		if ( ! isset( $_POST[ $form_name ] ) ||
 		     ! wp_verify_nonce( sanitize_key( wp_unslash( $_POST[ $form_name ] ) ), $form_name )
 		) {
-			echo 'there has been an error';
 			exit();
 		}
 
-
-		$user_email = ! empty( $_POST['user_email'] ) ? sanitize_email( wp_unslash( $_POST['user_email'] ) ) : null;
-		$title      = ! empty( $_POST['title'] ) ? sanitize_text_field( wp_unslash( $_POST['title'] ) ) : '';
-		$message    = ! empty( $_POST['message'] ) ? esc_textarea( wp_unslash( $_POST['message'] ) ) : '';
-
-//		$test_url = $this->base_url . '/latest.json';
-//		$test_url = add_query_arg( null, null, $test_url );
-//		$test_url = esc_url_raw( $test_url );
-//		$response = wp_remote_get( $test_url );
-
+		$email        = ! empty( $_POST['user_email'] ) ? sanitize_email( wp_unslash( $_POST['user_email'] ) ) : null;
+		$title        = ! empty( $_POST['title'] ) ? sanitize_text_field( wp_unslash( $_POST['title'] ) ) : '';
+		$message      = ! empty( $_POST['message'] ) ? esc_textarea( wp_unslash( $_POST['message'] ) ) : '';
 		$user_url     = $this->base_url . '/admin/users/list/active.json';
 		$api_key      = $this->options['api-key'];
 		$api_username = $this->options['publish-username'];
 
 
 		$user_url = add_query_arg( array(
-			'filter'       => urlencode( $user_email ),
+			'filter'       => urlencode( $email ),
 			'api_key'      => $api_key,
 			'api_username' => $api_username,
 		), $user_url );
@@ -118,11 +103,11 @@ class DiscourseRemoteMessage {
 			$response = json_decode( wp_remote_retrieve_body( $response ), true );
 
 			if ( 1 === count( $response ) && ! empty( $response[0]['username'] ) ) {
-				$discourse_username = $response[0]['username'];
-				$user_id = $response[0]['id'];
+				$username = $response[0]['username'];
+				$user_id  = $response[0]['id'];
 			} else {
 				$password = wp_generate_password( 15 );
-				$email    = $user_email;
+				$email    = $email;
 				$username = explode( '@', $email )[0];
 				$name     = $username;
 				$staged   = 'true';
@@ -148,63 +133,38 @@ class DiscourseRemoteMessage {
 
 				if ( $this->utilities->validate( $response ) ) {
 					$response = json_decode( wp_remote_retrieve_body( $response ), true );
-					$user_id = $response['user_id'];
+					$user_id  = $response['user_id'];
 
+				} else {
+					// do something
+					exit;
 				}
-
-//				$api_key_url = "$this->base_url/admin/users/{$user_id}/generate_api_key.json";
-//				$api_key_url = add_query_arg( array(
-//					'api_key'      => $api_key,
-//					'api_username' => $api_username,
-//				), $api_key_url );
-
-//				$response = wp_remote_post( $api_key_url );
-
-//				if ( $this->utilities->validate( $response ) ) {
-//					$response = json_decode( wp_remote_retrieve_body( $response ), true );
-//					if ( array_key_exists( 'api_key', $response ) ) {
-//						$user_api_key = $response['api_key']['key'];
-//						write_log($user_api_key);
-//					} else {
-						// Do something.
-//						exit();
-//					}
-//				}
-
-				$message_url = $this->base_url . '/posts';
-				$data = array(
-					'title' => 'This is a test message',
-					'raw' => $message,
-					'api_username' => $username,
-					'archetype' => 'private_message',
-					'target_usernames' => 'scossar,system',
-					'api_key' => $api_key,
-					'skip_validations' => 'true',
-				);
-
-				$response = wp_remote_post( $message_url, array(
-					'body' => $data
-				) );
-
-				if ( $this->utilities->validate( $response ) ) {
-//					$response = json_decode( $response );
-					write_log( $response );
-				}
-
 			}
 
+			$message_url = $this->base_url . '/posts';
+			$data        = array(
+				'title'            => $title,
+				'raw'              => $message,
+				'api_username'     => $username,
+				'archetype'        => 'private_message',
+				'target_usernames' => 'scossar,system',
+				'api_key'          => $api_key,
+				'skip_validations' => 'true',
+			);
+
+			$response = wp_remote_post( $message_url, array(
+				'body' => $data
+			) );
+
+			if ( ! $this->utilities->validate( $response ) ) {
+				// do something
+			}
 		}
-
-
-		// now find user or create staged user.
 
 		$referer_url = explode( '?', wp_get_referer() )[0];
 		$form_url    = home_url( $referer_url );
 		wp_safe_redirect( esc_url_raw( $form_url ) );
 
 		exit;
-
 	}
-
-
 }
