2d20a5a4e1bdd59201383bf6d89e23db67eb2274
scossar <scossar3@gmail.com>
2016-10-02 00:46:06 -0700

    Create staged user

--- js/handle-remote-message-submission.js
+++ js/handle-remote-message-submission.js
@@ -3,25 +3,22 @@ jQuery(document).ready(function () {
         var url = handle_remote_message_submission_script.ajaxurl,
             action = 'process_discourse_remote_message',
             $this = jQuery(this),
-            formName = $this.find('input[name=discourse_email_form_name]').val(),
+            formName = $this.find('input[name=discourse_remote_message_form_name]').val(),
             nonce = $this.find('#' + formName).val(),
 
             userEmail = $this.find('input[name=user_email]').val(),
-            title = $this.find('input[name=discourse_email_subject]').val(),
-            prefilledMessage = $this.find('input[name=discourse_email_message]').val(),
-            composedMessage = $this.find('textarea[name=discourse_email_message]').val();
-
+            title = $this.find('input[name=discourse_remote_message_title]').val(),
+            message = $this.find('input[name=discourse_remote_message]').val(),
             data = {
-                'formName': formName,
-                'nonce': nonce,
                 'action': action,
-                'userEmail': userEmail,
+                'form_name': formName,
+                'nonce': nonce,
+                'user_email': userEmail,
                 'title': title,
-                'prefilledMessage': prefilledMessage,
-                'composedMessage': composedMessage
+                'message': message
             };
 
-            console.log(url);
+
             jQuery.post(url, data, function(response) {
                 console.log(response);
             });
--- lib/discourse-remote-message.php
+++ lib/discourse-remote-message.php
@@ -12,19 +12,21 @@ class DiscourseRemoteMessage {
 		$this->utilities = $utilities;
 
 		add_action( 'init', array( $this, 'setup' ) );
-		add_action( 'wp_ajax_process_discourse_remote_message', array( $this, 'ajax_process_remote_message' ) );
-		add_action( 'wp_ajax_no_priv_process_discourse_remote_message', array(
+//		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+		add_shortcode( 'discourse_remote_message', array( $this, 'discourse_remote_message' ) );
+		add_action( 'wp_ajax_process_discourse_remote_message', array(
+			$this,
+			'ajax_process_remote_message'
+		) );
+		add_action( 'wp_ajax_nopriv_process_discourse_remote_message', array(
 			$this,
 			'ajax_process_remote_message'
 		) );
 	}
 
 	public function setup() {
-		$this->options = $this->utilities->get_options();
+		$this->options  = $this->utilities->get_options();
 		$this->base_url = $this->utilities->base_url( $this->options );
-
-		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
-		add_shortcode( 'discourse_remote_message', array( $this, 'discourse_remote_message' ) );
 	}
 
 	public function enqueue_scripts() {
@@ -33,42 +35,44 @@ class DiscourseRemoteMessage {
 		wp_enqueue_script( 'handle_remote_message_submission_js' );
 	}
 
-	public function discourse_remote_message_message( $atts ) {
+	public function discourse_remote_message( $atts ) {
 		$attributes = shortcode_atts( array(
-			'to'      => '',
-			'from'    => '',
-			'subject' => '',
-			'message'    => '',
+			'title'   => '',
+			'message' => '',
 		), $atts, 'discourse_remote_message' );
 
 		return $this->remote_message_form( $attributes );
 	}
 
-	protected function remote_message_form( $attributes ) {
+	public function remote_message_form( $attributes ) {
 		static $form_id = 0;
 		$form_id   = $form_id + 1;
 		$form_name = 'discourse_remote_message_form_' . (string) $form_id;
-		$title = ! empty( $attributes['title'] ) ? $attributes['title'] : null;
+		$title     = ! empty( $attributes['title'] ) ? $attributes['title'] : null;
 
 		$message = ! empty ( $attributes['message'] ) ? $attributes['message'] : null;
 		?>
 
-		<form action="<?php echo esc_url( admin_url( 'admin-post.php' ) ); ?>" method="post" class="discourse-remote-message">
+		<form action="<?php echo esc_url( admin_url( 'admin-ajax.php' ) ); ?>" method="post"
+		      class="discourse-remote-message">
 			<?php wp_nonce_field( $form_name, $form_name ); ?>
+			<input type="hidden" name="action" value="process_discourse_remote_message">
+			<input type="hidden" name="form_name" value="<?php echo $form_name; ?>">
 
-			<input type="hidden" name="discourse_remote_message_form_name" value="<?php echo $form_name; ?>">
-			if ( $title ) {
-				echo '<input type="hidden" name="discourse_remote_message_subject" value="' . sanitize_text_field( $title ) . '">';
-			}
-
-			?>
-			<label for="user_email"><?php esc_html_e( 'Your email address:' ) ?></label><br>
+			<label for="user_email"><?php esc_html_e( 'Your email address:' ); ?></label><br>
 			<input type="email" name="user_email"><br>
+			<?php if ( $title ) {
+				echo '<input type="hidden" name="title" value="' . sanitize_text_field( $title ) . '">';
+			} else {
+				echo '<label for="title">Subject:</label><br>';
+				echo '<input type="text" name="title" value="' . sanitize_text_field( $title ) . '">';
+			} ?>
 			<?php
 			if ( $message ) {
-				echo '<input type="hidden" name="discourse_remote_message" value="' . esc_textarea( $message ) . '">';
+				echo '<input type="hidden" name="message" value="' . esc_textarea( $message ) . '">';
 			} else {
-				echo '<textarea name="discourse_remote_message" cols="12" rows="5"></textarea>';
+				echo '<label for="message">Message:</label><br>';
+				echo '<textarea name="message" cols="12" rows="5"></textarea>';
 			}
 			?>
 			<input type="submit" value="Send us a message" id="<?php echo $form_name; ?>">
@@ -78,27 +82,91 @@ class DiscourseRemoteMessage {
 	}
 
 	public function ajax_process_remote_message() {
-		if ( ! empty( $_POST['formName'] ) && $form_name =  sanitize_key( wp_unslash( $_POST['formName'])))
-		if ( ! isset( $_POST['nonce'] ) ||
-		     ! wp_verify_nonce( sanitize_key( wp_unslash( $_POST['nonce'] ) ), $form_name ) ) {
+		$form_name = ! empty( $_POST['form_name'] ) ? sanitize_key( wp_unslash( $_POST['form_name'] ) ) : '';
+		if ( ! isset( $_POST[ $form_name ] ) ||
+		     ! wp_verify_nonce( sanitize_key( wp_unslash( $_POST[ $form_name ] ) ), $form_name )
+		) {
 			echo 'there has been an error';
 			exit();
 		}
 
-		$userEmail = ! empty( $_POST['from'] ) ? sanitize_email( wp_unslash( $_POST['userEmail'] ) ) : null;
-		$title = !empty( $_POST['subject'] ) ? sanitize_text_field( wp_unslash( $_POST['title'] ) ) : '';
-		if ( ! empty( $_POST['prefilledMessage'] ) ) {
-			$message = esc_textarea( wp_unslash( $_POST['prefilledMessage'] ) );
-		} elseif ( ! empty( $_POST['composedMessage'] ) ) {
-			$message = esc_textarea( wp_unslash( $_POST['composedMessage'] ) );
-		} else {
-			$message = '';
+
+		$user_email = ! empty( $_POST['user_email'] ) ? sanitize_email( wp_unslash( $_POST['user_email'] ) ) : null;
+		$title      = ! empty( $_POST['title'] ) ? sanitize_text_field( wp_unslash( $_POST['title'] ) ) : '';
+		$message    = ! empty( $_POST['message'] ) ? esc_textarea( wp_unslash( $_POST['message'] ) ) : '';
+
+//		$test_url = $this->base_url . '/latest.json';
+//		$test_url = add_query_arg( null, null, $test_url );
+//		$test_url = esc_url_raw( $test_url );
+//		$response = wp_remote_get( $test_url );
+
+		$user_url     = $this->base_url . '/admin/users/list/active.json';
+		$api_key      = $this->options['api-key'];
+		$api_username = $this->options['publish-username'];
+
+
+		$user_url = add_query_arg( array(
+			'filter'       => urlencode( $user_email ),
+			'api_key'      => $api_key,
+			'api_username' => $api_username,
+		), $user_url );
+
+		$user_url = esc_url_raw( $user_url );
+		$response = wp_remote_get( $user_url );
+
+		if ( $this->utilities->validate( $response ) ) {
+			$response = json_decode( wp_remote_retrieve_body( $response ), true );
+
+			if ( 1 === count( $response ) && ! empty( $response[0]['username'] ) ) {
+				$discourse_username = $response[0]['username'];
+			} else {
+				$password = wp_generate_password( 15 );
+				$email    = $user_email;
+				$username = explode( '@', $email )[0];
+				$name     = $username;
+				$staged   = 'true';
+
+				$create_user_url = $this->base_url . '/users';
+//				$create_user_url = add_query_arg( array(
+//					'api_key' => $api_key,
+//					'api_username' => $api_username,
+//				), $create_user_url );
+
+				$data = array(
+					'api_key' => $api_key,
+					'api_username' => $api_username,
+					'password' => $password,
+					'email'    => $email,
+					'username' => $username,
+					'name'     => $name,
+					'staged' => 'true',
+					'active' => 'false',
+				);
+
+				$post_options = array(
+//					'timeout' => 60,
+//					'method'  => 'PUT',
+					'body'    => $data,
+				);
+
+				wp_remote_post( $create_user_url, $post_options );
+
+				if ( $this->utilities->validate( $response ) ) {
+					write_log( $response );
+				}
+
+			}
+
 		}
+
 
 		// now find user or create staged user.
 
+		$referer_url = explode( '?', wp_get_referer() )[0];
+		$form_url    = home_url( $referer_url );
+		wp_safe_redirect( esc_url_raw( $form_url ) );
 
-		exit();
+		exit;
 
 	}
 
