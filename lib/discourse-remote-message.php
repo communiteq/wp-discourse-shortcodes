b04c224164e38d52083ba3b44cebf64b26033e38
scossar <scossar3@gmail.com>
2016-10-03 14:33:32 -0700

    Add validation and error messages

--- lib/discourse-remote-message.php
+++ lib/discourse-remote-message.php
@@ -40,38 +40,97 @@ class DiscourseRemoteMessage {
 
 	public function remote_message_form( $attributes ) {
 		static $form_id = 0;
-		$form_id    = $form_id + 1;
-		$form_name  = 'discourse_remote_message_form_' . (string) $form_id;
-		$title      = ! empty( $attributes['title'] ) ? $attributes['title'] : null;
-		$message    = ! empty ( $attributes['message'] ) ? $attributes['message'] : null;
-		$recipients = ! empty( $attributes['recipients'] ) ? $attributes['recipients'] : null;
+		$form_id               = $form_id + 1;
+		$form_name             = 'discourse_remote_message_form_' . (string) $form_id;
+		$title                 = ! empty( $attributes['title'] ) ? $attributes['title'] : null;
+		$message               = ! empty ( $attributes['message'] ) ? $attributes['message'] : null;
+		$recipients            = ! empty( $attributes['recipients'] ) ? $attributes['recipients'] : null;
+		$user_supplied_title   = '';
+		$user_supplied_subject = '';
+		$user_supplied_message = '';
 		?>
 
 		<form action="<?php echo esc_url( admin_url( 'admin-ajax.php' ) ); ?>" method="post"
 		      class="discourse-remote-message">
+			<?php
+			if ( isset( $_GET['form_errors'] ) ) {
+				if ( empty( $_GET['title'] ) ) {
+					$title_error_code = 'missing_title';
+					$this->form_errors()->add( $title_error_code, 'You must supply a subject for your message.' );
+				} else {
+					$user_supplied_title = urldecode( $_GET['title'] );
+				}
+
+				if ( empty( $_GET['message'] ) ) {
+					$message_error_code = 'missing_message';
+					$this->form_errors()->add( $message_error_code, 'You must supply a message.' );
+				} else {
+					$user_supplied_message = urldecode( $_GET['message'] );
+				}
+
+				if ( empty( $_GET['user_email'] ) ) {
+					$email_error_code = 'missing_email';
+					$this->form_errors()->add( $email_error_code, 'You must supply your email address.' );
+				} else {
+					$user_supplied_email = urldecode( $_GET['user_email'] );
+				}
+
+				if ( empty( $_GET['recipients'] ) ) {
+					$recipients_error_code = 'missing_recipients';
+					$this->form_errors()->add( $recipients_error_code, 'You must supply a recipient for your message' );
+				} else {
+					$user_supplied_recipients = urldecode( $_GET['recipients'] );
+				}
+			}
+			?>
+
+
 			<?php wp_nonce_field( $form_name, $form_name ); ?>
 			<input type="hidden" name="action" value="process_discourse_remote_message">
 			<input type="hidden" name="form_name" value="<?php echo $form_name; ?>">
 			<input type="hidden" name="recipients" value="<?php echo $recipients; ?>">
 
-			<label for="user_email"><?php esc_html_e( 'Your email address:' ); ?></label><br>
-			<input type="email" name="user_email">
+			<?php // Email field. ?>
+			<label for="user_email"><?php esc_html_e( 'Your email address:' ); ?></label>
+			<?php if ( isset( $email_error_code ) ) {
+				$error_message = $this->form_errors()->get_error_message( $email_error_code );
+				echo '<span class="error"><strong>Error</strong>: ' . $error_message . '</span>';
+			} ?>
+			<br>
+			<input type="email" name="user_email"
+			       value="<?php echo ! empty( $user_supplied_email ) ? $user_supplied_email : ''; ?>">
+
+			<?php // Subject field. ?>
 			<?php if ( $title ) {
 				echo '<input type="hidden" name="title" value="' . sanitize_text_field( $title ) . '">';
 			} else {
 				echo '<label for="title">Subject:</label>';
-				echo '<input type="text" name="title" value="' . sanitize_text_field( $title ) . '">';
-			} ?>
-			<?php
+				if ( isset( $title_error_code ) ) {
+					$error_message = $this->form_errors()->get_error_message( $title_error_code );
+					echo '<span class="error"><strong>Error</strong>: ' . $error_message . '</span>';
+				}
+				$user_supplied_title =! empty( $user_supplied_title ) ? $user_supplied_title : '';
+				echo '<input type="text" name="title" value="' . $user_supplied_title . '">';
+			}
+
+			// Message field.
 			if ( $message ) {
 				echo '<input type="hidden" name="message" value="' . esc_textarea( $message ) . '">';
 			} else {
 				echo '<label for="message">Message:</label>';
-				echo '<textarea name="message" cols="12" rows="5"></textarea>';
+				if ( isset( $message_error_code ) ) {
+					$error_message = $this->form_errors()->get_error_message( $message_error_code );
+					echo '<span class="error"><strong>Error</strong>: ' . $error_message . '</span>';
+				}
+				$user_supplied_message = ! empty( $user_supplied_message ) ? $user_supplied_message : '';
+				echo '<textarea name="message" cols="12" rows="5">' . $user_supplied_message . '</textarea>';
 			}
 			?>
+
+			<?php // Honeypot for bots. ?>
 			<label for="more_info">If you are a human, leave this field blank</label>
 			<input type="text" name="more_info" class="more_info">
+
 			<input type="submit" value="Send us a message" id="<?php echo $form_name; ?>">
 		</form>
 
@@ -90,13 +149,27 @@ class DiscourseRemoteMessage {
 		$referer_url = explode( '?', wp_get_referer() )[0];
 		$form_url    = home_url( $referer_url );
 
+		// The more_info field is a honeypot.
+		if ( ! empty( $_POST['more_info'] ) ) {
+
+			wp_safe_redirect( $form_url );
+			exit;
+		}
+
 		// Form values.
-		$email      = ! empty( $_POST['user_email'] ) ? sanitize_email( wp_unslash( $_POST['user_email'] ) ) : null;
+		$email      = ! empty( $_POST['user_email'] ) ? sanitize_email( wp_unslash( $_POST['user_email'] ) ) : '';
 		$title      = ! empty( $_POST['title'] ) ? sanitize_text_field( wp_unslash( $_POST['title'] ) ) : '';
 		$message    = ! empty( $_POST['message'] ) ? esc_textarea( wp_unslash( $_POST['message'] ) ) : '';
-		$recipients = ! empty( $_POST['recipients'] ) ? sanitize_text_field( wp_unslash( $_POST['recipients'] ) ) : null;
+		$recipients = ! empty( $_POST['recipients'] ) ? sanitize_text_field( wp_unslash( $_POST['recipients'] ) ) : '';
 
-		if ( ! $email && $title && $message && $recipients ) {
+		if ( ! $email || ! $title || ! $message || ! $recipients ) {
+			$form_url = add_query_arg( array(
+				'form_errors' => true,
+				'user_email'  => urlencode( $email ),
+				'title'       => urlencode( $title ),
+				'message'     => urlencode( $message ),
+				'recipients'  => urlencode( $recipients ),
+			), $form_url );
 
 			wp_safe_redirect( $form_url );
 			exit;
@@ -132,6 +205,12 @@ class DiscourseRemoteMessage {
 
 		wp_safe_redirect( $form_url );
 		exit;
+	}
+
+	protected function form_errors() {
+		static $wp_error;
+
+		return isset( $wp_error ) ? $wp_error : ( $wp_error = new \WP_Error() );
 	}
 
 	protected function discourse_username_from_email( $email, $api_key, $api_username ) {
