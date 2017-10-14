e726fe2715a805411c3dbb7e2aad82762efdc959
scossar <scossar3@gmail.com>
2017-10-14 14:46:18 -0700

    Remove rss related files

--- wp-discourse-shortcodes.php
+++ wp-discourse-shortcodes.php
@@ -27,10 +27,7 @@ function init() {
 		require_once( __DIR__ . '/lib/discourse-topics.php' );
 		require_once( __DIR__ . '/lib/discourse-groups.php' );
 		require_once( __DIR__ . '/lib/discourse-topic-formatter.php' );
-		require_once( __DIR__ . '/lib/discourse-rss.php' );
-		require_once( __DIR__ . '/lib/discourse-rss-formatter.php' );
 		require_once( __DIR__ . '/lib/shortcodes/discourse-topics-shortcode.php' );
-		require_once( __DIR__ . '/lib/shortcodes/discourse-rss-shortcode.php' );
 		require_once( __DIR__ . '/lib/shortcodes/discourse-groups-shortcode.php' );
 		require_once( __DIR__ . '/lib/shortcodes/discourse-link-shortcode.php' );
 		require_once( __DIR__ . '/lib/shortcodes/discourse-prefilled-message-shortcode.php' );
@@ -38,15 +35,12 @@ function init() {
 		new DiscourseShortcodes();
 		$topic_formatter   = new DiscourseTopicFormatter();
 		$discourse_topics  = new DiscourseTopics( $topic_formatter );
-		$rss_formatter     = new DiscourseRSSFormatter();
-		$discourse_rss     = new DiscourseRSS( $rss_formatter );
 		$discourse_link    = new DiscourseLink();
 		$prefilled_message = new DiscoursePrefilledMessage( $discourse_link );
 		$discourse_groups  = new DiscourseGroups( $discourse_link );
 		new DiscourseLinkShortcode( $discourse_link );
 		new DiscoursePrefilledMessageShortcode( $prefilled_message );
 		new DiscourseTopicsShortcode( $discourse_topics );
-		new DiscourseRSSShortcode( $discourse_rss );
 		new DiscourseGroupsShortcode( $discourse_groups );
 
 		if ( is_admin() ) {
